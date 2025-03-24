7e93f392a26821f7e6ee454c2eab3092e0c53ac0
Gabriel Philipe S <48845302+gabrielphilipes@users.noreply.github.com>
2025-03-24 10:19:29 -0300

    feat: recovery and change password pages

--- app/pages/auth/RecoveryPassword.vue
+++ app/pages/auth/RecoveryPassword.vue
@@ -1,5 +1,71 @@
+<script setup lang="ts">
+  import {
+    RecoveryPasswordSchema,
+    type RecoveryPasswordSchemaType
+  } from '~~/server/validations/auth'
+
+  const state = ref<RecoveryPasswordSchemaType>({
+    email: ''
+  })
+
+  const isValid = computed<boolean>(() => RecoveryPasswordSchema.safeParse(state.value).success)
+  const loading = ref<boolean>(false)
+
+  const handleSubmit = (e: SubmitEvent) => {
+    e.preventDefault()
+
+    if (!isValid.value) return
+
+    // TODO: Implementar a lógica de recuperação de senha
+  }
+</script>
+
 <template>
-  <div>
-    <h1>Recovery Password</h1>
-  </div>
+  <main class="h-screen flex flex-col md:items-center md:justify-center">
+    <section
+      class="flex-1 flex flex-col items-center justify-center w-10/12 mt-40 mx-auto md:max-w-xl md:m-0"
+    >
+      <header class="text-center block mb-10">
+        <h1 class="text-4xl font-bold">Recuperar senha 🔑</h1>
+        <p class="text-sm text-zinc-500">
+          Digite seu e-mail para receber as instruções de recuperação de senha
+        </p>
+      </header>
+
+      <div class="flex flex-col gap-5 w-full">
+        <UForm
+          :schema="RecoveryPasswordSchema"
+          :state="state"
+          @submit="handleSubmit"
+          class="flex flex-col gap-4"
+        >
+          <UFormField name="email" label="E-mail">
+            <UInput
+              v-model="state.email"
+              type="email"
+              :placeholder="`gabriel@${useAppConfig().site_name.toLowerCase()}.com`"
+              class="block"
+            />
+          </UFormField>
+
+          <UButton
+            type="submit"
+            label="Enviar instruções"
+            :disabled="!isValid"
+            :loading="loading"
+            class="justify-center font-bold"
+          />
+        </UForm>
+
+        <div class="flex justify-center text-xs font-medium mt-4">
+          <div class="flex items-center gap-1 text-zinc-400">
+            <p>Lembrou sua senha?</p>
+            <NuxtLink to="/login">Faça login!</NuxtLink>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <AuthFooter />
+  </main>
 </template>
--- server/validations/auth.ts
+++ server/validations/auth.ts
@@ -40,3 +40,31 @@ export const RegisterSchema = z
   })
 
 export type RegisterSchemaType = z.infer<typeof RegisterSchema>
+
+// Recovery Password
+export const RecoveryPasswordSchema = z.object({
+  email: z.string().email('Email inválido').min(1).max(255)
+})
+
+export type RecoveryPasswordSchemaType = z.infer<typeof RecoveryPasswordSchema>
+
+// Change Password
+export const ChangePasswordSchema = z
+  .object({
+    token: z.string().min(1, 'Token inválido'),
+    password: z
+      .string()
+      .min(8, 'A senha deve conter pelo menos 8 caracteres')
+      .max(255)
+      .regex(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&.])[A-Za-z\d@$!%*?&.]{8,}$/,
+        'A senha deve conter pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial'
+      ),
+    confirmPassword: z.string().min(8, 'A senha deve conter pelo menos 8 caracteres')
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ['confirmPassword'],
+    message: 'As senhas não conferem'
+  })
+
+export type ChangePasswordSchemaType = z.infer<typeof ChangePasswordSchema>
