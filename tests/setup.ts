87dfcbd01a2c78aa63cd834e87ae76742c90737b
Gabriel Philipe S <48845302+gabrielphilipes@users.noreply.github.com>
2025-04-01 19:48:10 -0300

    fix: enhance test setup with async-retry

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,5 +1,27 @@
 import { setup } from '@nuxt/test-utils'
+import { beforeAll } from 'vitest'
+import retry from 'async-retry'
 
 await setup({
   host: 'http://localhost:3000'
 })
+
+beforeAll(async () => {
+  const waitForWebServer = async () => {
+    const fetchStatusPage = async () => {
+      const response = await fetch('http://localhost:3000')
+
+      if (!response.ok) {
+        console.log('Waiting for web server to start...')
+        throw Error()
+      }
+    }
+
+    return retry(fetchStatusPage, {
+      retries: 30, // 30 retries
+      maxTimeout: 1000 // Timeout 1 second
+    })
+  }
+
+  await waitForWebServer()
+}, 30000)
--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,8 @@ import { defineVitestConfig } from '@nuxt/test-utils/config'
 
 export default defineVitestConfig({
   test: {
-    setupFiles: ['./tests/setup.ts']
+    setupFiles: ['./tests/setup.ts'],
+    hookTimeout: 30000,
+    testTimeout: 30000
   }
 })
