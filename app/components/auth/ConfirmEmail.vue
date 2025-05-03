13761ca4382e3c82a497263d73c0a8614071ed16
Gabriel Philipe S <48845302+gabrielphilipes@users.noreply.github.com>
2025-05-03 10:46:38 -0300

    feat: link front and back-end to request recovery password

--- app/components/auth/ConfirmEmail.vue
+++ app/components/auth/ConfirmEmail.vue
@@ -2,21 +2,24 @@
   const props = defineProps<{
     title?: string
     description?: string
+    icon?: string
   }>()
 
   const title = props.title ?? 'Confirme seu e-mail!'
   const description =
     props.description ??
     'Para confirmar sua conta e acessar o sistema, acesse seu e-mail e clique no link de confirmação.'
+
+  const icon = props.icon ?? 'material-symbols:mark-email-read'
 </script>
 
 <template>
   <UCard variant="soft">
     <div class="flex flex-col items-center gap-4 p-4">
-      <UIcon name="material-symbols:mark-email-read" class="text-4xl text-primary" />
+      <UIcon :name="icon" class="text-4xl text-primary" />
       <h1 class="text-lg font-medium text-center">{{ title }}</h1>
       <p class="text-sm text-center text-gray-600 dark:text-gray-400">
-        {{ description }}
+        <span v-html="description" />
       </p>
     </div>
   </UCard>
