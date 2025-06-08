dbae1cb43891cbef431a7d00127660e417bfe3e3
Gabriel Philipe S <48845302+gabrielphilipes@users.noreply.github.com>
2025-06-07 21:16:09 -0300

    feat(auth): create `SocialAuth` component for reusable social login buttons

---
+++ app/components/auth/SocialAuth.vue
@@ -0,0 +1,29 @@
+<template>
+  <UButton
+    type="button"
+    variant="link"
+    leading-icon="devicon:google"
+    label="Google"
+    class="!text-neutral-600 hover:text-neutral-600 hover:bg-neutral-100"
+    :disabled="true"
+  />
+
+  <UButton
+    type="button"
+    variant="link"
+    leading-icon="i-mdi-facebook"
+    label="Facebook"
+    class="!text-[#1877F2] hover:text-[#1877F2] hover:bg-[#1877F2]/10"
+    :disabled="true"
+  />
+
+  <a href="/oauth/github">
+    <UButton
+      type="button"
+      variant="link"
+      leading-icon="i-mdi-github"
+      label="GitHub"
+      class="!text-black hover:text-black hover:bg-black/5"
+    />
+  </a>
+</template>
--- app/pages/auth/register.vue
+++ app/pages/auth/register.vue
@@ -92,33 +92,7 @@
     <AuthConfirmCard v-if="isSuccess" />
 
     <div v-if="!isSuccess" class="flex items-center justify-center gap-2">
-      <UButton
-        type="button"
-        variant="link"
-        leading-icon="devicon:google"
-        label="Google"
-        class="!text-neutral-600 hover:text-neutral-600 hover:bg-neutral-100"
-        :disabled="true"
-      />
-
-      <UButton
-        type="button"
-        variant="link"
-        leading-icon="i-mdi-facebook"
-        label="Facebook"
-        class="!text-[#1877F2] hover:text-[#1877F2] hover:bg-[#1877F2]/10"
-        :disabled="true"
-      />
-
-      <a href="/oauth/github">
-        <UButton
-          type="button"
-          variant="link"
-          leading-icon="i-mdi-github"
-          label="GitHub"
-          class="!text-black hover:text-black hover:bg-black/5"
-        />
-      </a>
+      <AuthSocialAuth />
     </div>
 
     <USeparator v-if="!isSuccess" label="ou" :ui="{ label: 'text-neutral-500' }" />
